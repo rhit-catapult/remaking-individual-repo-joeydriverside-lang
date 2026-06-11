2beaacc79a3ba07689913625fc72ea6cf8a1ec3f
joeydriverside-lang <joeydriverside@gmail.com>
2026-06-11 19:40:23 +0000

    did test

--- 05-BouncingBall/BouncingBall.py
+++ 05-BouncingBall/BouncingBall.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import random
+import math
 
 
 # You will implement this module ENTIRELY ON YOUR OWN!
@@ -8,16 +9,41 @@ import random
 # TODO: Create a Ball class.
 # TODO: Possible member variables: screen, color, x, y, radius, speed_x, speed_y
 # TODO: Methods: __init__, draw, move
+class Ball:
+    def __init__(self, screen):
+        self.screen = screen
+        self.radius = random.randint(10, 20)
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        self.x = random.randint(self.radius, self.screen.get_width() - self.radius)
+        self.y = random.randint(self.radius, self.screen.get_height() - self.radius)
+        self.speed_x =  random.randint(5, 15)
+        self.speed_y =  random.randint(5, 15)
 
 
+    def draw(self):
+        pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.radius)
+    def move(self):
+        self.x += self.speed_x
+        self.y += self.speed_y
+        if self.x > self.screen.get_width():
+            self.speed_x *= -1
+        if self.y > self.screen.get_height():
+            self.speed_y *= -1
+        if self.x < 0:
+            self.speed_x *= -1
+        if self.y < 0:
+            self.speed_y *= -1
+        
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((300, 300))
+    screen = pygame.display.set_mode((800, 800))
     pygame.display.set_caption('Bouncing Ball')
     screen.fill(pygame.Color('gray'))
     clock = pygame.time.Clock()
 
-    # TODO: Create an instance of the Ball class called ball1
+    balls = []
+    for i in range(10):
+        balls.append(Ball(screen))
 
     while True:
         for event in pygame.event.get():
@@ -26,9 +52,9 @@ def main():
 
         clock.tick(60)
         screen.fill(pygame.Color('gray'))
-
-        # TODO: Move the ball
-        # TODO: Draw the ball
+        for ball in balls:
+            ball.move()
+            ball.draw()
 
         pygame.display.update()
 
